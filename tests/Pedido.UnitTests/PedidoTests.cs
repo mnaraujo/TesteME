e11f0d13d6ae05b2390db0da2a765266ee527361
Mônica Araujo <mnellytha@gmail.com>
2021-05-17 12:49:31 -0300

    commit inicial

---
+++ tests/Pedido.UnitTests/PedidoTests.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace Pedido.UnitTests
+{
+    public class PedidoTests
+    {
+        public void CreatePedido()
+        {
+            string produto = "nome produto";
+        }
+
+    }
+}
